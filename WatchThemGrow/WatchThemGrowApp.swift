5b7f1fbb9eedb2fbc369203d5f591f32c0292a0e
Tommi Saarikangas <tommi.saarikangas@reaktor.com>
2023-01-07 11:57:34 +0200

    Initial Commit

---
+++ WatchThemGrow/WatchThemGrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchThemGrowApp.swift
+//  WatchThemGrow
+//
+//  Created by Tommi Saarikangas on 7.1.2023.
+//
+
+import SwiftUI
+
+@main
+struct WatchThemGrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
